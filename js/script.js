36b639009fb23f29cedc0989316057ca61b575a0
delli patricio <delli.patricio.mayo@gmail.com>
2017-07-07 07:50:17 -0500

    Pagina-Version-13

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var nav = document.querySelector("nav ul");
+var boolean = false;
+
+function navBar() {
+  if (boolean == false) {
+    nav.style.margin = "0";
+    nav.style.transition = "1s";
+    boolean = true;
+  } else {
+    nav.style.margin = "0 0 0 -100%";
+    nav.style.transition = "1s";
+    boolean = false;
+  }
+  console.log(boolean);
+}
